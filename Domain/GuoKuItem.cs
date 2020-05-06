63ab51fb08a9bb00c43ff379d277d0a36b021b02
lemon <lemon@DESKTOP-UTT45GP>
2020-05-06 09:56:24 +0800

    取字符串-后面的凭证号

--- Domain/GuoKuItem.cs
+++ Domain/GuoKuItem.cs
@@ -33,17 +33,18 @@ namespace JournalVoucherAudit.Domain
             {
                 //转半角
                 var sbc = RemarkReason.ToSbc();
-                //取出数字
-                var number = sbc.GetNumber();
+                
                 //提取凭证号，格式为“报版面费4-1087”，4为四月，1087为凭证号
-                var pingzhen = number.Split('-');
-                //没有-
+                var pingzhen = sbc.Split('-');
+                //-号前的字符
                 var result = pingzhen[0];
-                //处理数字中有-，则取凭证号
+                //如果有-号，则取-号后的字符
                 if (pingzhen.Length == 2)
                 {
                     result = pingzhen[1];
                 }
+                //取出数字
+                var number = result.GetNumber();
                 return number;
             }
         }
