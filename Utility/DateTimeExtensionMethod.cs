1e3b920fe858d58414bb0ea80577b57cd7741a64
lemon <lemon@DESKTOP-UTT45GP>
2021-03-13 15:26:45 +0800

    优化月度最后一日算法

--- Utility/DateTimeExtensionMethod.cs
+++ Utility/DateTimeExtensionMethod.cs
@@ -5,24 +5,15 @@ namespace JournalVoucherAudit.Utility
     public static class DateTimeExtensionMethod
     {
         /// <summary>
-        /// 当月第一日
-        /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
-        public static DateTime FirstDayOfMonth(this DateTime date)
-        {
-            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
-            return firstDayOfMonth;
-        }
-        /// <summary>
         /// 当月最后一日
         /// </summary>
         /// <param name="date"></param>
         /// <returns></returns>
         public static DateTime LastDayOfMonth(this DateTime date)
         {
-            var firstDayOfMonth = FirstDayOfMonth(date);
-            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+
+            var nextMonth = new DateTime(date.Year, date.Month + 1, 1);
+            var lastDayOfMonth = nextMonth.AddDays(-1);
             return lastDayOfMonth;
         }
     }
