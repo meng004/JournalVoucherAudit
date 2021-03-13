2d551658e917dfe895e44314d25619426a011786
lemon <lemon@DESKTOP-UTT45GP>
2021-03-13 15:27:19 +0800

    月份最后一日的单元测试

---
+++ Utility.Tests/DateTimeExtensionMethodTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace JournalVoucherAudit.Utility.Tests
+{
+    [TestClass]
+    public class DateTimeExtensionMethodTests
+    {
+        [TestMethod]
+        public void LastDayOfMonth_Feb_Return28()
+        {
+            //arrange
+            var date = new DateTime(2021, 2, 1);
+            //act
+            var lastday = date.LastDayOfMonth();
+            //assert
+            Assert.AreEqual(28, lastday.Day);
+        }
+
+        [TestMethod]
+        public void LastDayOfMonth_July_Return31()
+        {
+            //arrange
+            var date = new DateTime(2021, 7, 1);
+            //act
+            var lastday = date.LastDayOfMonth();
+            //assert
+            Assert.AreEqual(31, lastday.Day);
+        }
+
+        [TestMethod]
+        public void LastDayOfMonth_August_Return31()
+        {
+            //arrange
+            var date = new DateTime(2021, 8, 1);
+            //act
+            var lastday = date.LastDayOfMonth();
+            //assert
+            Assert.AreEqual(31, lastday.Day);
+        }
+    }
+}
